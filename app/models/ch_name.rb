6869e86517ad1eba37aa38373f83df50e9e87c30
TimWu <mr.oldfive@gmail.com>
2023-05-03 14:04:21 +0800

    fix user model

--- app/models/ch_name.rb
+++
@@ -1,2 +0,0 @@
-class ChName < ApplicationRecord
-end
--- db/migrate/20230503060334_create_users.rb
+++ db/migrate/20230503060334_create_users.rb
@@ -1,6 +1,7 @@
-class CreateChNames < ActiveRecord::Migration[6.1]
+class CreateUsers < ActiveRecord::Migration[6.1]
   def change
-    create_table :ch_names do |t|
+    create_table :users do |t|
+      t.string :ch_name
       t.string :en_name
       t.string :phone_number
       t.datetime :b_day
